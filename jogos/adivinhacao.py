909fe5b1100326e97a2a6f04f64e81e186f1b590
John Erick <jhonerick147@outlook.com>
2021-04-06 11:33:30 -0300

    atividades alura

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -2,13 +2,29 @@ print('*********************************')
 print("Bem vindo no jogo de adivinhação!")
 print('*********************************')
 
-numero_secreto = 32
-chute_str = input('Digite o seu numero: ')
-print('Voce digitou {}'.format(chute_str))
+numero_secreto: int = 32
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(chute_str)
+while(rodada <= total_de_tentativas ):
+    print('Tentativas {} de {}'.format(rodada, total_de_tentativas))
+    chute_str = input('Digite o seu numero: ')
+    print('Voce digitou {}'.format(chute_str))
+    chute = int(chute_str)
 
-if numero_secreto == chute:
-    print('Voce acertou')
-else:
-    print('Voce errou o numero era {}'.format(numero_secreto))
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print('Voce acertou')
+    else:
+        if (maior):
+            print('voce errou o seu chute foi maior do que o numero secreto.')
+        elif(menor):
+            print('voce errou, seu chute foi menor do que o numero secreto.')
+
+
+    rodada = rodada + 1
+
+print('Fim de jogo.')
