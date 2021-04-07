6e34a566ceba64b9227d0da5e860d97a3c5de35e
John Erick <jhonerick147@outlook.com>
2021-04-07 10:35:30 -0300

    atividades alura

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -4,13 +4,17 @@ print('*********************************')
 
 numero_secreto: int = 32
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas ):
+
+for rodada in range(1, total_de_tentativas + 1):
     print('Tentativas {} de {}'.format(rodada, total_de_tentativas))
-    chute_str = input('Digite o seu numero: ')
+    chute_str = input('Digite o seu numero entre 1 e 100: ')
     print('Voce digitou {}'.format(chute_str))
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print('VOCE DEVE DIGITAR UM NUMERO DE 1 A 100...\n\n')
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -18,13 +22,11 @@ while(rodada <= total_de_tentativas ):
 
     if (acertou):
         print('Voce acertou')
+        break
     else:
         if (maior):
             print('voce errou o seu chute foi maior do que o numero secreto.')
         elif(menor):
             print('voce errou, seu chute foi menor do que o numero secreto.')
-
-
-    rodada = rodada + 1
 
 print('Fim de jogo.')
